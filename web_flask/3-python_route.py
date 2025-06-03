ef789456678ffec2c24fbc9ddc43f88f7e4419b7
angeumutoni <a.umutoni1@alustudent.com>
2025-06-03 12:39:49 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-""" Script that runs a flask application"""
+""" Starts a Flash Web Application Python is Cool"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ function that return " hello hbnb" """
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ function that displays hbnb!"""
+    """ Prints a Message when /hbnb is called """
     return 'HBNB'
-
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ function that returns "C" followed by the value of the text"""
-    return 'C {}'.format(text.replace('_', ''))
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
 
-@app.route('/pythons', strict_slashes=False)
-@app.route('/pythons/<text>', strict_slashes=False)
-def python_is_cool(text = 'is  cool'):
-    """ function that returns python followed by the value of the text"""
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is_cool'):
+    """ Prints a Message when /python is called """
+    return "Python " + text.replace('_', ' ')
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
